dd763ce3406cf34431427e08be6684687fb9d12a
tmepple <tmepple@users.noreply.github.com>
2022-02-04 07:36:39 -0500

    Use logflare_api_client with relaxed Mint version dep

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule LogflareLogger.MixProject do
       {:typed_struct, ">= 0.0.0"},
       {:bertex, "~> 1.3"},
       {:etso, "~> 0.1.2"},
-      {:logflare_api_client, "~> 0.3.2"},
+      {:logflare_api_client, git: "https://github.com/tmepple/logflare_api_client.git"},
 
       # Test and Dev
       {:placebo, "~> 2.0", only: :test},
